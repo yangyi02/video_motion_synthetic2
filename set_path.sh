69e4c5f8a5cc066ddc7821a5173dc40f84074a8e
yangyi02 <yangyi02@gmail.com>
2017-07-08 11:24:10 -0700

    fix small bug for ubuntu

--- set_path.sh
+++ set_path.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# export PYTHONPATH=/home/yi/code/video_motion_synthetic2:$PYTHONPATH
-export PYTHONPATH=/Users/yiyang/Desktop/video_motion_synthetic2:$PYTHONPATH
+export PYTHONPATH=/home/yi/code/video_motion_synthetic2:$PYTHONPATH
